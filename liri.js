07c482bcc7e5cdce30dfc3f6109abc202e13d0eb
unknown <kevhemsoth@gmail.com>
2019-03-29 21:18:50 -0400

    concert-this start

--- liri.js
+++ liri.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
 var keys = require('./keys.js');
+var axios = require('axios');
+var moment = require('moment');
 
 //var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 
 if (command === 'concert-this') {
     console.log(command);
+    let artist = process.argv[3];
+    axios
+      .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+      .then(function(res) {
+        console.log('---Venue Name---');
+        console.log(res.data.venue);
+        console.log('---Location---');
+        console.log('City: ' + res.venue.city);
+        console.log('State: ' + res.venue.region);
+        console.log('Country: ' + res.venue.country);
+        console.log('---Date---');
+        console.log(moment(res.data.datetime).format(MM/DD/YYYY));
+      })
 }
 else if (command === 'spotify-this-song') {
     console.log(command);
