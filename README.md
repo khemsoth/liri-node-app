0b46795b4965af4e86fb756ccd10723e27e7ed97
unknown <kevhemsoth@gmail.com>
2019-04-04 22:45:39 -0400

    readme/do-what-it-says

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # liri-node-app
+
+The liri node app is uses a couple different APIs in order to retrieve and show some info. 
+It uses the Bands-In-Town, Spotify, and OMDB APIs to retrieve the info. 
+
+If you do not have a spotify API key yet, sign up for one here: https://developer.spotify.com/my-applications/#!/
+
+How to use: 
+
+-Create a new .env file using the env.template file. Insert your own Spotify keys
+-Open cmd and install all of the npm packages
+
+Spotify: 
+-Type 'node liri spotify-this-song "<song name>"'
+-Will return artist, song name, preview link, and album
+
+Concert:
+-Type: 'node liri concert-this "<artist>"'
+-Will return name of venue, location, and date
+
+Movie: 
+-Type: 'node liri movie-this "<movie title>"'
+-Will return title, year, IMDB rating, Rotten Tomatoes rating, country origin, language, plot, and actors in the movie
+
+Do what it says:
+-This function will take whatever is in the random.txt file, and execute it in the terminal
+-Type: 'node liri do-what-it-says'
+-Will return the result of the random.txt file
--- liri.js
+++ liri.js
@@ -3,11 +3,11 @@ var keys = require('./keys.js');
 var axios = require('axios');
 var moment = require('moment');
 var Spotify = require('node-spotify-api');
+var fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 
-if (command === 'concert-this') {
-    console.log(command);
+function concert() {
     let artist = process.argv[3];
     axios
       .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
@@ -20,28 +20,28 @@ if (command === 'concert-this') {
           console.log('----------------');
       }})
 }
-else if (command === 'spotify-this-song') {
-    console.log(command);
+
+function music() {
     let song = process.argv[3];
     spotify.search({type: 'track', query: song, limit: 10}, function(err, data){
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-//-----------All data in an array------------ 
         let info = data.tracks;
         for (var i = 0; i < info.items.length; i++) {
           console.log('\r\nArtist: ' + info.items[i].artists[0].name);
           console.log('Song: ' + info.items[i].name);
           console.log('Preview: ' + info.items[i].external_urls.spotify);
           console.log('Album: ' + info.items[i].album.name);
+          console.log('============================');
         }
     })
 }
-else if (command === 'movie-this') {
-    console.log(command);
-    let movie = process.argv[3];
+
+function movie() {
+    let title = process.argv[3];
     axios  
-      .get('http://www.omdbapi.com/?apikey=trilogy&t=' + movie)
+      .get('http://www.omdbapi.com/?apikey=trilogy&t=' + title)
       .then(function(res){
           console.log('Movie Title: ' + res.data.Title);
           console.log('Year of Release: ' + res.data.Released);
@@ -56,9 +56,79 @@ else if (command === 'movie-this') {
           console.log('Movie Plot: ' + res.data.Plot);
           console.log('Actors/Actresses: ' + res.data.Actors);
       }
-      )}
-else if(command === 'do-what-it-says'){
+      )
+}
+
+if (command === 'concert-this') {
     console.log(command);
+    concert();
+}
+else if (command === 'spotify-this-song') {
+    console.log(command);
+    music();
+}
+else if (command === 'movie-this') {
+    console.log(command);
+    movie();
+    }
+else if(command === 'do-what-it-says'){
+    
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+        console.log(data);
+        var ranData = data.split(',');
+        if (err) {
+            return console.log(err);
+        }
+        else if (ranData[0] === 'concert-this') {
+            let artist = ranData[1];
+            axios
+              .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=keithsappid")
+              .then(function(res) {
+                let info = res.data;
+                console.log(info);
+                for (var i = 0; i < res.data.length; i++) {
+                  console.log('Venue: ' + res.data[i]);
+                  console.log('Location: ' + res.data[i].venue.city + ',' + info[i].venue.region + ',' + info[i].venue.country);
+                  console.log('Date: ' + moment(res.data[i].datetime).format('MM DD YYYY'));
+                  console.log('----------------');
+              }})      
+         }
+        else if (ranData[0] === 'spotify-this-song') {
+            let song = ranData[1];
+            spotify.search({type: 'track', query: song, limit: 10}, function(err, data){
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                let info = data.tracks;
+                for (var i = 0; i < info.items.length; i++) {
+                  console.log('\r\nArtist: ' + info.items[i].artists[0].name);
+                  console.log('Song: ' + info.items[i].name);
+                  console.log('Preview: ' + info.items[i].external_urls.spotify);
+                  console.log('Album: ' + info.items[i].album.name);
+                  console.log('============================');
+                }
+            })
+        }
+        else if (ranData[0] === 'movie-this') {
+            let title = ranData[1];
+            axios  
+              .get('http://www.omdbapi.com/?apikey=trilogy&t=' + title)
+              .then(function(res){
+                  console.log('Movie Title: ' + res.data.Title);
+                  console.log('Year of Release: ' + res.data.Released);
+                  console.log('IMDB Rating: ' + res.data.imdbRating);
+                  if (res.data.Ratings.Source === 'Rotten Tomatoes'){
+                    console.log('Rotten Tomatoes Rating: ' + res.data.Ratings.Source);
+                  } else{
+                      console.log('Rotten Tomatoes Rating: N/A');
+                  }
+                  console.log('Country of Production: ' + res.data.Country);
+                  console.log('Movie Language: ' + res.data.Language);
+                  console.log('Movie Plot: ' + res.data.Plot);
+                  console.log('Actors/Actresses: ' + res.data.Actors);
+              }
+              )            }
+    })
 
 }
 else {
